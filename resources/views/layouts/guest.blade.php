f8aadf4d065b788aedf60fabd78c25b8156003b4
unknown <01ahmadmin9e@gmail.com>
2026-01-13 09:46:49 +0700

    redesign ui2

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,7 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="icon" type="image/png" href="{{ asset('images/ppd.png') }}">
 
-        <title>Login - Absen QR</title>
+        <title>Login - DIGIDAS</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">

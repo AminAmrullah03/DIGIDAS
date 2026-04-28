4851b886886c4da38558dec32862455c24c052d9
unknown <01ahmadmin9e@gmail.com>
2026-04-29 00:00:04 +0700

    add manifest.json

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="icon" type="image/png" href="{{ asset('images/logo1.png') }}">
+        <link rel="manifest" href="{{ asset('manifest.json') }}">
 
         <title>DIGIDAS</title>
 

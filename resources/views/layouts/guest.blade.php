e1130c19e4a8a96681d8c01ce067dce922ff470c
unknown <01ahmadmin9e@gmail.com>
2026-04-30 12:31:29 +0700

    fix manifest

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,6 +5,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="icon" type="image/png" href="{{ asset('images/ppd.png') }}">
+        <link rel="manifest" href="/manifest.json">
+        <meta name="theme-color" content="#059669">
+        <meta name="mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-title" content="DIGIDAS">
+        <link rel="apple-touch-icon" href="/images/icon-192.png">
 
         <title>Login - DIGIDAS</title>
 
